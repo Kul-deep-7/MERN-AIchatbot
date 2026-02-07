06457e54ec5b8f382ce7b768fc974633076834e7
Kul-deep-7 <sahiljangid321@gmail.com>
2026-02-07 15:59:00 +0530

    feat: initialize backend server with Express and dotenv

---
+++ backend/app.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import cors from 'cors';
+
+let app = express();
+
+app.use(cors())
---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+
+let app = express()
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000')
+})
